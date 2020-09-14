8a6bd6bf0dbb70cf05e35ab0660955c968b01650
luisren64 <lrenteria5@miners.utep.edu>
2020-09-13 23:28:51 -0600

    Starting with basic command accepting

---
+++ shell/shell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import res
+
+
+#method to accept input
+def main():
+    while True:
+        command = input("$ ")
+        if command == "exit":
+            break
+        else if command == "help":
+            print("Type 'exit' to exit. That is all.")
+        else if cd in command:
+            
+        else:
+            execute(command)
+
+
+
+def execute(command):
+    
+            
