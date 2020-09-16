0f172b3807176cfe9a405584089184e81ee87c9d
luisren64 <lrenteria5@miners.utep.edu>
2020-09-15 23:51:28 -0600

    Beginning to understand pipes and forks. No working code.

--- shell/shell.py
+++ shell/shell.py
@@ -9,17 +9,54 @@ import res
 def main():
     while True:
         command = input("$ ")
+        args = command.split()
+        
         if command == "exit":
             break
-        else if command == "help":
+        elif command == "help":
             print("Type 'exit' to exit. That is all.")
-        else if cd in command:
-            
+
+        elif args[0] == "cd":
+                try:
+                    os.chdir(args[1])
+                except FileNotFoundError:
+                    print("cd "+args[1]+". File or directory not found.")
+
+        elif "|" in args:
+            print("Going to pipes")
+            handle_pipes(command)
+
+        elif not in command:
+            #empty input. Nothing to do
+        
         else:
             execute(command)
-
-
-
-def execute(command):
+    sys.exit(1)
     
+def execute(command):    
+    pid = os.fork()
+
+    if pid < 0:
+        print("Fork failure")
+        sys.exit(1)
+    
+    
+def handle_pipes(command):
+    
+    r, w = os.pipe()
+
+    pid = os.fork()
+
+    if pid < 0:
+        print("Fork failure")
+        sys.exit(1)
+
+    elif pid >0:
+        #parent
+        
+    else:
+        #child
+
+        args = command.split()
+        if args[0] == "ls":
             
